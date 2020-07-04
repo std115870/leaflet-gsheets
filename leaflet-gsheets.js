941afeedf0b4bc5ff27988d531d7303127d60a34
Konstantinos Douvaras <44108679+std115870@users.noreply.github.com>
2020-07-04 19:00:17 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -13,7 +13,7 @@ function init() {
   var polyURL =
     "https://docs.google.com/spreadsheets/d/1-zWIQqpzFrO6aZPvVTBMsLGJD6DS--g-0ntz1Yd00To/edit?usp=sharing";
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/1mRV8yYeA3NrpMbbxrd8f7l5kpySRNIIkYPNIF3JSF6g/edit?usp=sharing";
 
   Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
